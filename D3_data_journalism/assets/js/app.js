691c85f5a995ff74f53bc114cd6a9c3c3794da03
Veronica Nixon <vsnixon@gmail.com>
2021-01-09 17:21:48 -0700

    Adds draft code for interactive functionality. Breaks pretty much everything temporarily.

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -15,10 +15,10 @@ var svg = d3
 // Declare object variable to hold margins
 var margin =
 {
-    top: 50,
-    bottom: 50,
-    left: 50,
-    right: 50
+    top: 100,
+    bottom: 100,
+    left: 100,
+    right: 100
 };
 
 // Set up chart height and width
@@ -30,6 +30,11 @@ var chartHeight = svgHeight - margin.top - margin.bottom;
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+// Declare variables to hold user's dataset choice.
+// Defaults are set to display healthcare vs. poverty on page load.
+var chosenXAxis = "poverty";
+var chosenYAxis = "healthcare"
+
 // Import data
 d3.csv("/assets/data/data.csv").then(function (data, err) {
     if (err) throw err;
@@ -39,24 +44,36 @@ d3.csv("/assets/data/data.csv").then(function (data, err) {
     data.forEach(function(d) {
         d.poverty = +d.poverty;
         d.healthcare = +d.healthcare;
+        d.age = +d.age;
+        d.smokes = +d.smokes;
     });
     
-    // Declares x and y scaling rules
-    var x_Scale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.poverty)])
+    // Declares function to update x scaling rule based on user choice
+    function getXScale(data, chosenXAxis) 
+    {
+        var x_Scale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d[chosenXAxis])])
         .range([0, chartWidth]);
-
-    var y_Scale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.healthcare)])
+        return x_Scale;
+    }
+    
+    // Declares function to update y scaling rule based on user choice
+    function getYScale(data, chosenYAxis) 
+    {
+        var y_Scale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d[chosenYAxis])])
         .range([chartHeight, 0]);
+        return y_Scale;
+    }
+    
 
     // Appends circles to chart group and position based on poverty and healthcare data values
     chartGroup.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", data => x_Scale(data.poverty))
-        .attr("cy", data => y_Scale(data.healthcare))
+        .attr("cx", getXScale(data, chosenXAxis))
+        .attr("cy", getYScale(data, chosenYAxis))
         .attr("r", 15)
         .attr("fill", "#34a1eb")
     
@@ -67,13 +84,13 @@ d3.csv("/assets/data/data.csv").then(function (data, err) {
         .append("text")
         .text(data => data.abbr)
         .attr("text-anchor", "middle")
-        .attr("x", data => x_Scale(data.poverty))
-        .attr("y", data => y_Scale(data.healthcare)+5)
+        .attr("x", getXScale(data, chosenXAxis))
+        .attr("y", getYScale(data, chosenYAxis))
         .attr("fill", "white");
 
     // Declares scaled axes objects
-    var y_axis = d3.axisLeft(y_Scale).ticks(5);
-    var x_axis = d3.axisBottom(x_Scale).ticks(5);
+    var y_axis = d3.axisLeft(getYScale(data, chosenYAxis)).ticks(5);
+    var x_axis = d3.axisBottom(getXScale(data, chosenXAxis)).ticks(5);
 
     // Appends axes
     chartGroup.append("g")
@@ -92,6 +109,12 @@ d3.csv("/assets/data/data.csv").then(function (data, err) {
         .attr("x", 0)
         .attr("y", 20)
         .text("Poverty Index");
+    
+    labelsGroup.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", 0)
+        .attr("y", 40)
+        .text("Age");
 
     // Append y-axis labels
     chartGroup.append("text")
@@ -102,6 +125,15 @@ d3.csv("/assets/data/data.csv").then(function (data, err) {
         .attr("dy", "1em")
         .classed("axis-text", true)
         .text("Healthcare Index");
+    
+    chartGroup.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "0em")
+        .classed("axis-text", true)
+        .text("Smoking Index");
 
     // Logs any errors to the console
 }).catch(function (error) {
